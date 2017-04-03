3a1cd534e7a7d306382750c2154e50afac6d953d
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-04-03 12:26:30 +0200

    Add json serde to flink

--- src/main/java/ch/hsr/markovshield/flink/MarkovShieldAnalyser.java
+++ src/main/java/ch/hsr/markovshield/flink/MarkovShieldAnalyser.java
@@ -1,6 +1,8 @@
 package ch.hsr.markovshield.flink;
 
 import ch.hsr.markovshield.models.Session;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -8,9 +10,10 @@ import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
-import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.util.serialization.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 
@@ -26,10 +29,36 @@ public class MarkovShieldAnalyser {
         properties.setProperty("group.id", "test");
         DataStreamSource<ObjectNode> stream = env
                 .addSource(new FlinkKafkaConsumer010<>("MarkovLogins", new JSONKeyValueDeserializationSchema(false), properties));
+        ObjectMapper mapper = new ObjectMapper();
 
-        SingleOutputStreamOperator<String> sessionStream = stream.map(session -> session.toString());
-        FlinkKafkaProducer010<String> producer = new FlinkKafkaProducer010<String>("broker:9092", "xxx",new SimpleStringSchema());
+        SingleOutputStreamOperator<Session> sessionStream = stream.map(jsonNodes -> mapper.treeToValue(jsonNodes.get("value"), Session.class));
+
+
+
+        FlinkKafkaProducer010<Session> producer = new FlinkKafkaProducer010<Session>("broker:9092", "xxx", new KeyedSerializationSchema<Session>() {
+
+            @Override
+            public byte[] serializeKey(Session session) {
+                return session.getSession().getBytes();
+            }
+
+            @Override
+            public byte[] serializeValue(Session session) {
+                try {
+                    return mapper.writeValueAsBytes(session);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+                return new byte[1];
+            }
+
+            @Override
+            public String getTargetTopic(Session session) {
+                return "xxx";
+            }
+        });
         sessionStream.addSink(producer);
+
         sessionStream.print();
         stream.print();
 
