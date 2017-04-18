0bc40fc7256a1baee37514490c0def13fed3db86
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-04-18 09:21:52 +0200

    Trying some more stuff

--- src/test/java/io/confluent/examples/streams/WordCountLambdaIntegrationTest.java
+++ src/test/java/io/confluent/examples/streams/WordCountLambdaIntegrationTest.java
@@ -1,4 +1,18 @@
-package ch.hsr.markovshield.kafkastream;
+/**
+ * Copyright 2016 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.confluent.examples.streams;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -7,38 +21,46 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.integration.utils.IntegrationTestUtils;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.test.TestUtils;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-
-import io.confluent.examples.streams.kafka.EmbeddedSingleNodeKafkaCluster;
 import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import io.confluent.examples.streams.kafka.EmbeddedSingleNodeKafkaCluster;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class MarkovShieldClickstreamsTest {
+/**
+ * End-to-end integration test based on {@link }, using an embedded Kafka
+ * cluster.
+ *
+ * See {@link } for further documentation.
+ *
+ * See {@link } for the equivalent Scala example.
+ *
+ * Note: This example uses lambda expressions and thus works with Java 8+ only.
+ */
+public class WordCountLambdaIntegrationTest {
+
     @ClassRule
     public static final EmbeddedSingleNodeKafkaCluster CLUSTER = new EmbeddedSingleNodeKafkaCluster();
+
     private static final String inputTopic = "inputTopic";
-    private static final String rekeyedIntermediateTopic = "rekeyedIntermediateTopic";
     private static final String outputTopic = "outputTopic";
 
     @BeforeClass
     public static void startKafkaCluster() throws Exception {
-        CLUSTER.createTopic(inputTopic, 2, 1);
-        CLUSTER.createTopic(rekeyedIntermediateTopic);
+        CLUSTER.createTopic(inputTopic);
         CLUSTER.createTopic(outputTopic);
     }
 
@@ -52,13 +74,10 @@ public class MarkovShieldClickstreamsTest {
         );
         List<KeyValue<String, Long>> expectedWordCounts = Arrays.asList(
             new KeyValue<>("hello", 1L),
-            new KeyValue<>("kafka", 1L),
-            new KeyValue<>("streams", 1L),
             new KeyValue<>("all", 1L),
             new KeyValue<>("streams", 2L),
             new KeyValue<>("lead", 1L),
             new KeyValue<>("to", 1L),
-            new KeyValue<>("kafka", 2L),
             new KeyValue<>("join", 1L),
             new KeyValue<>("kafka", 3L),
             new KeyValue<>("summit", 1L),
@@ -72,40 +91,32 @@ public class MarkovShieldClickstreamsTest {
         //
         // Step 1: Configure and start the processor topology.
         //
-        // This code is identical to what's shown in WordCountLambdaExample, with the exception of
-        // the call to `purgeLocalStreamsState()`, which we need only for cleaning up previous runs of
-        // this integration test.
-        //
         final Serde<String> stringSerde = Serdes.String();
         final Serde<Long> longSerde = Serdes.Long();
 
         Properties streamsConfiguration = new Properties();
         streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-integration-test");
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
-        streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, CLUSTER.zookeeperConnect());
         streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        // The commit interval for flushing records to state stores and downstream must be lower than
+        // this integration test's timeout (30 secs) to ensure we observe the expected processing results.
+        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
         streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        // Explicitly place the state directory under /tmp so that we can remove it via
-        // `purgeLocalStreamsState` below.  Once Streams is updated to expose the effective
-        // StreamsConfig configuration (so we can retrieve whatever state directory Streams came up
-        // with automatically) we don't need to set this anymore and can update `purgeLocalStreamsState`
-        // accordingly.
-        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/kafka-streams");
-
-        // Remove any state from previous test runs
-        IntegrationTestUtils.purgeLocalStreamsState(streamsConfiguration);
+        // Use a temporary directory for storing state, which will be automatically removed after the test.
+        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory().getAbsolutePath());
 
         KStreamBuilder builder = new KStreamBuilder();
 
         KStream<String, String> textLines = builder.stream(inputTopic);
+
         Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
+
         KStream<String, Long> wordCounts = textLines
             .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
-            .map((key, word) -> new KeyValue<>(word, word))
-            // Required only in 0.10.0 to re-partition the data because we re-keyed the stream in the
-            // `map` step.  Upcoming Kafka 0.10.1 does this automatically (no need for `through`).
-            .through(rekeyedIntermediateTopic).groupByKey().count("Counts")
+            // no need to specify explicit serdes because the resulting key and value types match our default serde settings
+            .groupBy((key, word) -> word)
+            .count("Counts")
             .toStream();
 
         wordCounts.to(stringSerde, longSerde, outputTopic);
@@ -122,7 +133,7 @@ public class MarkovShieldClickstreamsTest {
         producerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
         producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        IntegrationTestUtils.produceValuesSynchronously(inputTopic, inputValues, producerConfig, Time.SYSTEM);
+        IntegrationTestUtils.produceValuesSynchronously(inputTopic, inputValues, producerConfig);
 
         //
         // Step 3: Verify the application's output data.
@@ -136,7 +147,7 @@ public class MarkovShieldClickstreamsTest {
         List<KeyValue<String, Long>> actualWordCounts = IntegrationTestUtils.waitUntilMinKeyValueRecordsReceived(consumerConfig,
             outputTopic, expectedWordCounts.size());
         streams.close();
-        assertThat(actualWordCounts, equalTo(expectedWordCounts));
+        assertThat(actualWordCounts).containsExactlyElementsOf(expectedWordCounts);
     }
 
 }
