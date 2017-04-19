6320403640eebdf657458aa84e3177ae051c3782
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-04-19 11:00:57 +0200

    Restructure project

---
+++ core/src/main/java/ch/hsr/markovshield/models/TransitionModel.java
@@ -0,0 +1,94 @@
+package ch.hsr.markovshield.models;
+
+import ch.hsr.markovshield.ml.TransitionMatrix;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
+
+@JsonAutoDetect (fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class TransitionModel {
+
+    private final TransitionMatrix transitionMatrix;
+    private final UrlStore urlStore;
+    private final Date timeCreated;
+
+    public TransitionModel(TransitionMatrix transitionMatrix, UrlStore urlStore) {
+        this.transitionMatrix = transitionMatrix;
+        this.urlStore = urlStore;
+        timeCreated = Date.from(Instant.now());
+
+    }
+
+    @JsonCreator
+    private TransitionModel(@JsonProperty ("transitionMatrix") TransitionMatrix transitionMatrix,
+                            @JsonProperty ("urlStore") UrlStore urlStore,
+                            @JsonProperty ("timeCreated") Date timeCreated) {
+        this.transitionMatrix = transitionMatrix;
+        this.urlStore = urlStore;
+        this.timeCreated = timeCreated;
+    }
+
+    private UrlStore getUrlStore() {
+        return this.urlStore;
+    }
+
+    private TransitionMatrix getTransitionMatrix() {
+        return this.transitionMatrix;
+    }
+
+    public Date getTimeCreated() {
+        return timeCreated;
+    }
+
+    public double getProbabilityForClick(Click currentClick, Click newClick) {
+        return getProbabilityForClick(currentClick.getUrl(), newClick.getUrl());
+    }
+
+    public double getProbabilityForClick(String currentUrl, String newUrl) {
+        Optional<Integer> sourceIndex = getIndexByUrl(currentUrl);
+        Optional<Integer> targetIndex = getIndexByUrl(newUrl);
+        double transistionProbability;
+        if (sourceIndex.isPresent() && targetIndex.isPresent()) {
+            transistionProbability = transitionMatrix.get(sourceIndex.get(), targetIndex.get());
+        } else {
+            transistionProbability = 0;
+        }
+        return transistionProbability;
+    }
+
+    private Optional<Integer> getIndexByUrl(String url) {
+        return this.urlStore.get(url);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TransitionModel that = (TransitionModel) o;
+        return Objects.equals(transitionMatrix, that.transitionMatrix) &&
+            Objects.equals(urlStore, that.urlStore) &&
+            Objects.equals(timeCreated, that.timeCreated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transitionMatrix, urlStore, timeCreated);
+    }
+
+    @Override
+    public String toString() {
+        return "TransitionModel{" +
+            "transitionMatrix=" + transitionMatrix +
+            ", urlStore=" + urlStore +
+            ", timeCreated=" + timeCreated +
+            '}';
+    }
+}
---
+++ kafka-stream/src/main/java/ch/hsr/markovshield/kafkastream/MarkovModelGenerator.java
@@ -0,0 +1,80 @@
+package ch.hsr.markovshield.kafkastream;
+
+import ch.hsr.markovshield.ml.MarkovChainWithMatrix;
+import ch.hsr.markovshield.models.FrequencyModel;
+import ch.hsr.markovshield.models.UserModel;
+import ch.hsr.markovshield.utils.JsonPOJOSerde;
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.io.IOException;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.Thread.sleep;
+
+public class MarkovModelGenerator {
+
+    public static void main(final String[] args) throws IOException, InterruptedException {
+        produceInputs();
+    }
+
+    private static void produceInputs() throws IOException, InterruptedException {
+        final List<UserModel> userModels = new LinkedList<>();
+
+
+        final List<String> users = new LinkedList<String>();
+        users.add("Kilian");
+        users.add("Philip");
+        users.add("Matthias");
+        users.add("Ivan");
+        users.add("Kilian");
+
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        for (String user : users) {
+            int transisitionModelRating = random.nextInt(1, 100);
+            int frequencyModelRating = random.nextInt(1, 100);
+            UserModel userModel = new UserModel(user,
+                MarkovChainWithMatrix.train(Collections.emptyList()),
+                new FrequencyModel(frequencyModelRating, Date.from(
+                    Instant.now())));
+            userModels.add(userModel);
+        }
+
+
+        final Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonPOJOSerde.class);
+
+        properties.put("schema.registry.url", "http://localhost:8081");
+
+
+        SchemaRegistryClient client = new CachedSchemaRegistryClient("http://localhost:8081", 100);
+
+        final KafkaProducer<String, UserModel> modelProducer = new KafkaProducer<String, UserModel>(properties,
+            Serdes.String().serializer(),
+            new JsonPOJOSerde<UserModel>(UserModel.class).serializer());
+
+        final String modelTopic = "MarkovUserModels";
+
+        sleep(1000);
+        for (UserModel userModel : userModels) {
+            modelProducer.send(new ProducerRecord<String, UserModel>(modelTopic,
+                userModel.getUserId().toString(),
+                userModel));
+            modelProducer.flush();
+        }
+
+    }
+
+}
