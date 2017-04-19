f0f4b32d795ef44e2de5a70c320f8a0e396bb4e3
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-04-19 13:04:51 +0200

    Refactorings

--- kafka-stream/src/main/java/ch/hsr/markovshield/kafkastream/MarkovShieldClickstreams.java
+++ kafka-stream/src/main/java/ch/hsr/markovshield/kafkastream/MarkovShieldClickstreams.java
@@ -32,44 +32,80 @@ public class MarkovShieldClickstreams {
     public static final String MARKOV_USER_MODEL_TOPIC = "MarkovUserModels";
     public static final String MARKOV_CLICK_TOPIC = "MarkovClicks";
     public static final String MARKOV_CLICK_STREAM_TOPIC = "MarkovClickStreams";
-    private static final String MARKOV_CONFIG_TOPIC = "MarkovUrlConfig";
-    private static final String MARKOV_CLICK_STREAM_ANALYSIS_TOPIC = "MarkovClickStreamAnalysis";
+    public static final String MARKOV_CLICK_STREAM_ANALYSIS_TOPIC = "MarkovClickStreamAnalysis";
 
     public static void main(final String[] args) throws Exception {
-        final Properties streamsConfiguration = new Properties();
-
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, KAFKA_JOB_NAME);
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER);
-        streamsConfiguration.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY);
-        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1);
-
-
+        final Properties streamsConfiguration = getStreamConfiguration();
         final KStreamBuilder builder = new KStreamBuilder();
+        final KTable<String, Session> sessions = getSessionTable(builder);
+        final KTable<String, UserModel> userModels = getUserModelTable(builder);
+        final KStream<String, Click> views = getClickStream(builder);
 
-        final KTable<String, Session> sessions = builder.table(Serdes.String(),
-            new JsonPOJOSerde<>(Session.class),
-            MARKOV_LOGIN_TOPIC,
-            "MarkovLoginStore");
-        final KTable<String, UserModel> userModels = builder.table(Serdes.String(),
-            new JsonPOJOSerde<>(UserModel.class),
-            MARKOV_USER_MODEL_TOPIC,
-            "MarkovUserModelStore");
+        KTable<String, ClickStream> clickstreams = aggregateClicks(sessions, views);
 
+        KStream<String, ClickStream> stringValidationClickStreamKStream = filterByThreshold(clickstreams);
+
+        KStream<String, ValidationClickStream> clickStreamsWithModel = addModelToClickStreams(userModels,
+            stringValidationClickStreamKStream);
+
+        outputClickStreams(clickstreams);
+
+        outputClickstreamsForAnalysis(clickStreamsWithModel);
+
+        stringValidationClickStreamKStream.print();
         userModels.foreach((key, value) -> System.out.println("UserModel: " + key + " " + value.toString()));
-
         sessions.foreach((key, value) -> System.out.println("Session: " + key + " " + value.toString()));
-
-        final KStream<String, Click> views = builder.stream(Serdes.String(),
-            new JsonPOJOSerde<>(Click.class),
-            MARKOV_CLICK_TOPIC);
-
         views.foreach((key, value) -> System.out.println("Click: " + key + " " + value.toString()));
+        clickStreamsWithModel.print();
 
+        startStreamingApp(streamsConfiguration, builder);
+    }
 
-        KTable<String, ClickStream> clickstreams = views.leftJoin(sessions,
+    private static void startStreamingApp(Properties streamsConfiguration, KStreamBuilder builder) {
+        final KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
+        streams.cleanUp();
+        streams.start();
+        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
+        addShutdown(streams);
+    }
+
+    private static void addShutdown(KafkaStreams streams) {
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+    private static void outputClickstreamsForAnalysis(KStream<String, ValidationClickStream> clickStreamsWithModel) {
+        clickStreamsWithModel.to(Serdes.String(),
+            new JsonPOJOSerde<>(ValidationClickStream.class),
+            MARKOV_CLICK_STREAM_ANALYSIS_TOPIC);
+    }
+
+    private static void outputClickStreams(KTable<String, ClickStream> clickstreams) {
+        clickstreams.to(Serdes.String(), new JsonPOJOSerde<>(ClickStream.class), MARKOV_CLICK_STREAM_TOPIC);
+    }
+
+    private static KStream<String, ValidationClickStream> addModelToClickStreams(KTable<String, UserModel> userModels, KStream<String, ClickStream> stringValidationClickStreamKStream) {
+        return stringValidationClickStreamKStream.mapValues(
+            ValidationClickStream::fromClickStream).leftJoin(
+            userModels,
+            (ValidationClickStream clickStream, UserModel userModel) -> {
+                clickStream.setUserModel(userModel);
+                return clickStream;
+            },
+            Serdes.String(),
+            new JsonPOJOSerde<>(ValidationClickStream.class));
+    }
+
+    private static KStream<String, ClickStream> filterByThreshold(KTable<String, ClickStream> clickstreams) {
+        return clickstreams
+            .toStream((s, clickStream) -> clickStream.getUserName()).filter((s, validationClickStream) -> {
+                Optional<Boolean> overThreshold = validationClickStream.lastClick()
+                    .map(MarkovShieldClickstreams::isUrlOverThreshold);
+                return overThreshold.orElse(false);
+            });
+    }
+
+    private static KTable<String, ClickStream> aggregateClicks(KTable<String, Session> sessions, KStream<String, Click> views) {
+        return views.leftJoin(sessions,
             (view, session) -> {
                 String newUserName;
                 if (session != null) {
@@ -91,41 +127,39 @@ public class MarkovShieldClickstreams {
                     Lists.newLinkedList(concat(clickStream.getClicks(), v1.getClicks())));
             }, "MarkovClickStreamAggregation"
         );
+    }
 
-        KStream<String, ClickStream> stringValidationClickStreamKStream = clickstreams
-            .toStream((s, clickStream) -> clickStream.getUserName()).filter((s, validationClickStream) -> {
-                Optional<Boolean> overThreshold = validationClickStream.lastClick()
-                    .map(MarkovShieldClickstreams::isUrlOverThreshold);
-                return overThreshold.orElse(false);
-            });
+    private static KStream<String, Click> getClickStream(KStreamBuilder builder) {
+        return builder.stream(Serdes.String(),
+            new JsonPOJOSerde<>(Click.class),
+            MARKOV_CLICK_TOPIC);
+    }
 
-        stringValidationClickStreamKStream.print();
+    private static Properties getStreamConfiguration() {
+        final Properties streamsConfiguration = new Properties();
 
-        KStream<String, ValidationClickStream> clickStreamsWithModel = stringValidationClickStreamKStream.mapValues(
-            ValidationClickStream::fromClickStream).leftJoin(
-            userModels,
-            (ValidationClickStream clickStream, UserModel userModel) -> {
-                clickStream.setUserModel(userModel);
-                return clickStream;
-            },
-            Serdes.String(),
-            new JsonPOJOSerde<>(ValidationClickStream.class));
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, KAFKA_JOB_NAME);
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER);
+        streamsConfiguration.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY);
+        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1);
+        return streamsConfiguration;
+    }
 
-        clickstreams.to(Serdes.String(), new JsonPOJOSerde<>(ClickStream.class), MARKOV_CLICK_STREAM_TOPIC);
+    private static KTable<String, UserModel> getUserModelTable(KStreamBuilder builder) {
+        return builder.table(Serdes.String(),
+            new JsonPOJOSerde<>(UserModel.class),
+            MARKOV_USER_MODEL_TOPIC,
+            "MarkovUserModelStore");
+    }
 
-        clickStreamsWithModel.print();
-        clickStreamsWithModel.to(Serdes.String(),
-            new JsonPOJOSerde<>(ValidationClickStream.class),
-            MARKOV_CLICK_STREAM_ANALYSIS_TOPIC);
-
-
-        final KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
-
-        streams.cleanUp();
-        streams.start();
-
-        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    private static KTable<String, Session> getSessionTable(KStreamBuilder builder) {
+        return builder.table(Serdes.String(),
+            new JsonPOJOSerde<>(Session.class),
+            MARKOV_LOGIN_TOPIC,
+            "MarkovLoginStore");
     }
 
     private static Boolean isUrlOverThreshold(Click click) {
