4a270b7ed40cbaa0c26b85f46a7f8015353b394d
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-04-19 13:24:38 +0200

    some further refactorings

--- kafka-stream/src/main/java/ch/hsr/markovshield/kafkastream/MarkovShieldClickstreams.java
+++ kafka-stream/src/main/java/ch/hsr/markovshield/kafkastream/MarkovShieldClickstreams.java
@@ -10,6 +10,7 @@ import ch.hsr.markovshield.utils.JsonPOJOSerde;
 import com.google.common.collect.Lists;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
@@ -33,6 +34,13 @@ public class MarkovShieldClickstreams {
     public static final String MARKOV_CLICK_TOPIC = "MarkovClicks";
     public static final String MARKOV_CLICK_STREAM_TOPIC = "MarkovClickStreams";
     public static final String MARKOV_CLICK_STREAM_ANALYSIS_TOPIC = "MarkovClickStreamAnalysis";
+    private static final Serde<String> stringSerde = Serdes.String();
+    private static final JsonPOJOSerde<ValidationClickStream> validationClickStreamSerde = new JsonPOJOSerde<>(
+        ValidationClickStream.class);
+    private static final JsonPOJOSerde<Click> clickSerde = new JsonPOJOSerde<>(Click.class);
+    private static final JsonPOJOSerde<Session> sessionSerde = new JsonPOJOSerde<>(Session.class);
+    private static final JsonPOJOSerde<UserModel> userModelSerde = new JsonPOJOSerde<>(UserModel.class);
+    private static final JsonPOJOSerde<ClickStream> clickStreamSerde = new JsonPOJOSerde<>(ClickStream.class);
 
     public static void main(final String[] args) throws Exception {
         final Properties streamsConfiguration = getStreamConfiguration();
@@ -43,7 +51,7 @@ public class MarkovShieldClickstreams {
 
         KTable<String, ClickStream> clickstreams = aggregateClicks(sessions, views);
 
-        KStream<String, ClickStream> stringValidationClickStreamKStream = filterByThreshold(clickstreams);
+        KStream<String, ClickStream> stringValidationClickStreamKStream = filterForAnalysis(clickstreams);
 
         KStream<String, ValidationClickStream> clickStreamsWithModel = addModelToClickStreams(userModels,
             stringValidationClickStreamKStream);
@@ -74,92 +82,112 @@ public class MarkovShieldClickstreams {
     }
 
     private static void outputClickstreamsForAnalysis(KStream<String, ValidationClickStream> clickStreamsWithModel) {
-        clickStreamsWithModel.to(Serdes.String(),
-            new JsonPOJOSerde<>(ValidationClickStream.class),
-            MARKOV_CLICK_STREAM_ANALYSIS_TOPIC);
+        clickStreamsWithModel
+            .to(stringSerde,
+                validationClickStreamSerde,
+                MARKOV_CLICK_STREAM_ANALYSIS_TOPIC);
     }
 
-    private static void outputClickStreams(KTable<String, ClickStream> clickstreams) {
-        clickstreams.to(Serdes.String(), new JsonPOJOSerde<>(ClickStream.class), MARKOV_CLICK_STREAM_TOPIC);
+    private static void outputClickStreams(KTable<String, ClickStream> clickStreams) {
+        clickStreams.to(stringSerde, clickStreamSerde, MARKOV_CLICK_STREAM_TOPIC);
     }
 
     private static KStream<String, ValidationClickStream> addModelToClickStreams(KTable<String, UserModel> userModels, KStream<String, ClickStream> stringValidationClickStreamKStream) {
-        return stringValidationClickStreamKStream.mapValues(
-            ValidationClickStream::fromClickStream).leftJoin(
-            userModels,
-            (ValidationClickStream clickStream, UserModel userModel) -> {
-                clickStream.setUserModel(userModel);
-                return clickStream;
-            },
-            Serdes.String(),
-            new JsonPOJOSerde<>(ValidationClickStream.class));
+        return stringValidationClickStreamKStream
+            .mapValues(ValidationClickStream::fromClickStream)
+            .leftJoin(userModels,
+                (ValidationClickStream clickStream, UserModel userModel) -> {
+                    clickStream.setUserModel(userModel);
+                    return clickStream;
+                },
+                stringSerde,
+                validationClickStreamSerde);
     }
 
-    private static KStream<String, ClickStream> filterByThreshold(KTable<String, ClickStream> clickstreams) {
+    private static KStream<String, ClickStream> filterForAnalysis(KTable<String, ClickStream> clickstreams) {
         return clickstreams
-            .toStream((s, clickStream) -> clickStream.getUserName()).filter((s, validationClickStream) -> {
-                Optional<Boolean> overThreshold = validationClickStream.lastClick()
-                    .map(MarkovShieldClickstreams::isUrlOverThreshold);
-                return overThreshold.orElse(false);
-            });
+            .toStream((s, clickStream) -> clickStream.getUserName())
+            .filter((s, validationClickStream) -> isClickStreamToBeAnalysed(validationClickStream));
     }
 
-    private static KTable<String, ClickStream> aggregateClicks(KTable<String, Session> sessions, KStream<String, Click> views) {
-        return views.leftJoin(sessions,
-            (view, session) -> {
-                String newUserName;
-                if (session != null) {
-                    newUserName = session.getUserName();
-                } else {
-                    newUserName = USER_NOT_FOUND;
-                }
-                return new ClickStream(newUserName, view.getSessionUUID(), Collections.singletonList(view));
-            }, Serdes.String(), new JsonPOJOSerde<>(Click.class)
-        ).groupByKey(Serdes.String(), new JsonPOJOSerde<>(ClickStream.class)).reduce(
-            (clickStream, v1) -> {
-                String userName = clickStream.getUserName();
-                if (clickStream.getUserName().equals(USER_NOT_FOUND) && !(clickStream.getUserName()
-                    .equals(v1.getUserName()))) {
-                    userName = v1.getUserName();
-                }
-                return new ClickStream(userName,
-                    clickStream.getSessionUUID(),
-                    Lists.newLinkedList(concat(clickStream.getClicks(), v1.getClicks())));
-            }, "MarkovClickStreamAggregation"
-        );
+    private static boolean isClickStreamToBeAnalysed(ClickStream validationClickStream) {
+        Optional<Boolean> overThreshold = validationClickStream
+            .lastClick()
+            .map(MarkovShieldClickstreams::isUrlOverThreshold);
+        return overThreshold.orElse(false);
+    }
+
+    private static KTable<String, ClickStream> aggregateClicks(KTable<String, Session> sessions, KStream<String, Click> clicks) {
+        return clicks
+            .leftJoin(sessions,
+                MarkovShieldClickstreams::getInitialClickStream,
+                stringSerde,
+                clickSerde)
+            .groupByKey(stringSerde,
+                clickStreamSerde)
+            .reduce(
+                MarkovShieldClickstreams::reduceClickStreams, "MarkovClickStreamAggregation"
+            );
     }
 
     private static KStream<String, Click> getClickStream(KStreamBuilder builder) {
-        return builder.stream(Serdes.String(),
-            new JsonPOJOSerde<>(Click.class),
-            MARKOV_CLICK_TOPIC);
+        return builder
+            .stream(stringSerde,
+                clickSerde,
+                MARKOV_CLICK_TOPIC);
     }
 
     private static Properties getStreamConfiguration() {
         final Properties streamsConfiguration = new Properties();
-
         streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, KAFKA_JOB_NAME);
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER);
         streamsConfiguration.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY);
-        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, stringSerde.getClass().getName());
+        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, stringSerde.getClass().getName());
         streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1);
         return streamsConfiguration;
     }
 
     private static KTable<String, UserModel> getUserModelTable(KStreamBuilder builder) {
-        return builder.table(Serdes.String(),
-            new JsonPOJOSerde<>(UserModel.class),
-            MARKOV_USER_MODEL_TOPIC,
-            "MarkovUserModelStore");
+        return builder
+            .table(stringSerde,
+                userModelSerde,
+                MARKOV_USER_MODEL_TOPIC,
+                "MarkovUserModelStore");
     }
 
     private static KTable<String, Session> getSessionTable(KStreamBuilder builder) {
-        return builder.table(Serdes.String(),
-            new JsonPOJOSerde<>(Session.class),
-            MARKOV_LOGIN_TOPIC,
-            "MarkovLoginStore");
+        return builder
+            .table(stringSerde,
+                sessionSerde,
+                MARKOV_LOGIN_TOPIC,
+                "MarkovLoginStore");
+    }
+
+    private static ClickStream getInitialClickStream(Click click, Session session) {
+        String newUserName;
+        if (session != null) {
+            newUserName = session.getUserName();
+        } else {
+            newUserName = USER_NOT_FOUND;
+        }
+        return new ClickStream(newUserName, click.getSessionUUID(), Collections.singletonList(click));
+    }
+
+    private static ClickStream reduceClickStreams(ClickStream clickStream, ClickStream anotherClickStream) {
+        String userName = clickStream.getUserName();
+        if (isUserNameIsNotSet(clickStream, anotherClickStream)) {
+            userName = anotherClickStream.getUserName();
+        }
+        return new ClickStream(userName,
+            clickStream.getSessionUUID(),
+            Lists.newLinkedList(concat(clickStream.getClicks(), anotherClickStream.getClicks())));
+    }
+
+    private static boolean isUserNameIsNotSet(ClickStream clickStream, ClickStream anotherClickStream) {
+        return clickStream.getUserName().equals(USER_NOT_FOUND)
+            && !(clickStream.getUserName().equals(anotherClickStream.getUserName()));
     }
 
     private static Boolean isUrlOverThreshold(Click click) {
