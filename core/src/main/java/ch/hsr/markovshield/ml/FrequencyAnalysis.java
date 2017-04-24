621f792d42a67bb8cfb4dc30a0dc9a32ca606c55
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-04-24 16:27:37 +0200

    Calcuate frequencies

--- core/src/main/java/ch/hsr/markovshield/ml/FrequencyAnalysis.java
+++ core/src/main/java/ch/hsr/markovshield/ml/FrequencyAnalysis.java
@@ -17,15 +17,20 @@ public class FrequencyAnalysis {
     public static FrequencyModel train(Iterable<ClickStream> stream) {
 
         HashMap<String, HashMap<String, Double>> clickCountMatrix = new HashMap<>();
-
+        Map<String, Integer> urlMap = new HashMap<>();
+        Map<String, Integer> sessionMap = new HashMap<>();
         stream.forEach(clickStream -> {
             for (Click click :
                 clickStream.getClicks()) {
+                if(!urlMap.containsKey(click.getUrl())){
+                    urlMap.put(click.getUrl(), urlMap.size());
+                }
                 updateClickCount(clickCountMatrix, click);
             }
+            sessionMap.put(clickStream.getSessionUUID(),sessionMap.size());
         });
-        Map<String, Integer> urlMap = getMappings(clickCountMatrix);
-        FrequencyMatrix clickProbabilityMatrix = calculateFrequencies(clickCountMatrix, urlMap);
+        double[][] clicks = new double[urlMap.size()][sessionMap.size()];
+        FrequencyMatrix clickProbabilityMatrix = calculateFrequencies(clicks, urlMap);
         return new FrequencyModel(clickProbabilityMatrix, new UrlStore(urlMap));
     }
 
@@ -39,34 +44,23 @@ public class FrequencyAnalysis {
         return urlMapping;
     }
 
-    private static FrequencyMatrix calculateFrequencies(HashMap<String, HashMap<String, Double>> clickCountMatrix, Map<String, Integer> urlMap) {
-
+    private static FrequencyMatrix calculateFrequencies(double[][] data, Map<String, Integer> urlMap) {
         FrequencyMatrix clickFrequencyMatrix = new FrequencyMatrix(urlMap.size());
-        clickCountMatrix.forEach((url, stringIntegerHashMap) -> {
-            int size = stringIntegerHashMap.size();
-            Collection<Double> values = stringIntegerHashMap.values();
-            double[] data = new double[size];
-
-            Iterator<Double> iterator = values.iterator();
-            for(int i = 0; i < size; i++){
-                data[i] = iterator.next();
-            }
-            // obtain data here
-            DescriptiveStatistics da = new DescriptiveStatistics(data);
+        for (Map.Entry<String, Integer> entry:urlMap.entrySet()
+             ) {
+            DescriptiveStatistics da = new DescriptiveStatistics(data[entry.getValue()]);
             double firstQuartile = da.getPercentile(25);
             double thirdQuartile = da.getPercentile(75);
             double iqr = thirdQuartile - firstQuartile;
             double lowerBound = firstQuartile - 1.5 * iqr;
             double upperBound = thirdQuartile + 1.5 * iqr;
-
-
-
-            stringIntegerHashMap.forEach((s1, integer) -> addToFrequencyMatrix(clickFrequencyMatrix,
-                url,
+            addToFrequencyMatrix(clickFrequencyMatrix,
+                entry.getKey(),
                 lowerBound,
                 upperBound,
-                urlMap));
-        });
+                urlMap);
+
+        }
         return clickFrequencyMatrix;
     }
 
