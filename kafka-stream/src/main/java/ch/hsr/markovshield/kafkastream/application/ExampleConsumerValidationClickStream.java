e705ea53b9520dc122e1663c0ca3e7724a7f933d
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-05-31 09:22:49 +0200

    modify output of exampleConsumerValidationClickStream

--- kafka-stream/src/main/java/ch/hsr/markovshield/kafkastream/application/ExampleConsumerValidationClickStream.java
+++ kafka-stream/src/main/java/ch/hsr/markovshield/kafkastream/application/ExampleConsumerValidationClickStream.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 import java.util.Properties;
 
 public class ExampleConsumerValidationClickStream {
+
     public static void main(final String[] args) throws IOException, InterruptedException {
         produceInputs();
     }
@@ -24,17 +25,43 @@ public class ExampleConsumerValidationClickStream {
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG,"Example");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "Example");
         final KafkaConsumer<String, ValidationClickStream> clickConsumer = new KafkaConsumer<>(properties,
             Serdes.String().deserializer(),
             new JsonPOJOSerde<>(ValidationClickStream.class, JsonPOJOSerde.MARKOV_SHIELD_SMILE).deserializer());
         clickConsumer.subscribe(Collections.singletonList(MarkovTopics.MARKOV_CLICK_STREAM_ANALYSIS_TOPIC));
         boolean running = true;
+        long sum = 0;
+        int count = 0;
         try {
             while (running) {
                 ConsumerRecords<String, ValidationClickStream> records = clickConsumer.poll(1000);
-                for (ConsumerRecord<String, ValidationClickStream> record : records){
-                    System.out.println(Instant.now().toEpochMilli() - record.value().timeStampOfLastClick().toInstant().toEpochMilli());
+                sum = 0;
+                long max = 0;
+                count = 0;
+                for (ConsumerRecord<String, ValidationClickStream> record : records) {
+                    long diff1 = Instant.now()
+                        .toEpochMilli() - record.value()
+                        .timeStampOfLastClick()
+                        .toInstant()
+                        .toEpochMilli();
+
+                    System.out.println(record.value().getSessionUUID() + ": " + diff1 + "        " + (Instant.now()
+                        .toEpochMilli() - record.value()
+                        .getClicks()
+                        .get(record.value().getClicks().size() - 1)
+                        .getKafkaFirstProcessedDate()
+                        .toInstant()
+                        .toEpochMilli()));
+                    if(diff1>max){
+                        max = diff1;
+                    }
+                    sum += diff1;
+                    count++;
+                    if(count % 100 == 0){
+                        System.out.println("--------- average-------------: " + sum / count);
+                        System.out.println("--------- max-------------: " + max);
+                    }
                 }
             }
         } finally {
